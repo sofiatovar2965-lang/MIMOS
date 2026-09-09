232c3f957aff8c93a56c8265205f782b2aff53ba
Sofía <sofiatovar2965@gmail.com>
2026-09-09 10:39:10 -0500

    actualizando

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ export const obtenerPorEmail= async(email)=>{
     .from("usuarios")
     .select("*")
     .eq ('email', email)
-    .single();
+    .maybeSingle();
     return{data,error};
 };
 
@@ -62,4 +62,25 @@ export const eliminarUsuario = async (id) => {
 
    
 
+
+// 2. Función específica para los usuarios autenticados con Google
+export const crearUsuarioGoogle = async ({ nombre, email, googleId, avatar = null, rol = 'cliente' }) => {
+    const { data, error } = await supabase
+        .from('usuarios')
+        .insert({
+            nombre,
+            email,
+            contrasena: null,        // No requiere contraseña
+            rol,
+            isVerified: true,      // Google ya validó este correo
+            googleId,
+            avatar,
+            codigoVerificacion: null,
+            codigoVerificacionExpiracion: null
+        })
+        .select('id, nombre, email, rol, avatar')
+        .single();
+
+    return { data, error };
+};
 
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { registro,login,verificarCuenta } from "../controllers/Auth.js";
 import { forgotPassword,verifyCode } from "../controllers/Recuperar.js";
+import { autenticarConGoogle } from "../controllers/googleauth.controller.js";
+
 
 const router = express.Router();
 
@@ -12,5 +14,8 @@ router.post('/verify-account', verificarCuenta);
 //Ruta de olvido de contraseña
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-code', verifyCode);
+
+// Endpoint: POST /api/auth/google
+router.post("/google", autenticarConGoogle);
 
 export default router;
